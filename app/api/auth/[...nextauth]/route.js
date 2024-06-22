3c84039ddba48835061d0d762d2627352575a57c
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-06-22 09:16:06 -1100

    fix error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import User from "@models/user";
+import User from "@models/User";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -11,30 +11,25 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
   callbacks: {
-    async singIn({ profile }) {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        // check if a user alredy exists
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new user
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
-            emial: profile.email,
+            email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture,
           });
@@ -42,7 +37,7 @@ const handler = NextAuth({
 
         return true;
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
